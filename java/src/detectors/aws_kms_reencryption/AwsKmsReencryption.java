310408f62a43222f0e085ecc2ccce3e627650be4
rathore25Test1 <nitish.rathore25+test1@gmail.com>
2024-06-13 12:09:44 -0700

    Update AwsKmsReencryption.java

--- java/src/detectors/aws_kms_reencryption/AwsKmsReencryption.java
+++ java/src/detectors/aws_kms_reencryption/AwsKmsReencryption.java
@@ -12,30 +12,30 @@ import software.amazon.awssdk.services.kms.model.EncryptRequest;
 import software.amazon.awssdk.services.kms.model.ReEncryptRequest;
 
 public class AwsKmsReencryption {
-    // {fact rule=aws-kms-reencryption@v1.0 defects=1}
-    public void withoutReEncryptNoncompliant(KmsClient client, SdkBytes encryptedContent) {
-
-        DecryptRequest decryptRequest = DecryptRequest.builder()
-                .ciphertextBlob(encryptedContent)
-                .build();
-        SdkBytes plaintext = client.decrypt(decryptRequest).plaintext();
-        // Noncompliant: client-side decrypt immediately followed by encrypt.
-        EncryptRequest encryptRequest = EncryptRequest.builder()
-                .keyId("my-key-id")
-                .plaintext(plaintext)
-                .build();
-        client.encrypt(encryptRequest);
-    }
-    // {/fact}
-
-    // {fact rule=aws-kms-reencryption@v1.0 defects=0}
-    public void withReEncryptCompliant(KmsClient client, SdkBytes encryptedContent) {
-        // Compliant: uses a ReEncryptRequest which runs server-side.
-        ReEncryptRequest req = ReEncryptRequest.builder()
-                .ciphertextBlob(encryptedContent)
-                .destinationKeyId("my-key-id")
-                .build();
-        client.reEncrypt(req).ciphertextBlob();
-    }
-    // {/fact}
+        // {fact rule=aws-kms-reencryption@v1.0 defects=1}
+        public void withoutReEncryptNoncompliant(KmsClient client, SdkBytes encryptedContent) {
+    
+            DecryptRequest decryptRequest = DecryptRequest.builder()
+                    .ciphertextBlob(encryptedContent)
+                    .build();
+            SdkBytes plaintext = client.decrypt(decryptRequest).plaintext();
+            // Noncompliant: client-side decrypt immediately followed by encrypt.
+            EncryptRequest encryptRequest = EncryptRequest.builder()
+                    .keyId("my-key-id")
+                    .plaintext(plaintext)
+                    .build();
+            client.encrypt(encryptRequest);
+        }
+        // {/fact}
+    
+        // {fact rule=aws-kms-reencryption@v1.0 defects=0}
+        public void withReEncryptCompliant(KmsClient client, SdkBytes encryptedContent) {
+            // Compliant: uses a ReEncryptRequest which runs server-side.
+            ReEncryptRequest req = ReEncryptRequest.builder()
+                    .ciphertextBlob(encryptedContent)
+                    .destinationKeyId("my-key-id")
+                    .build();
+            client.reEncrypt(req).ciphertextBlob();
+        }
+        // {/fact}
 }
